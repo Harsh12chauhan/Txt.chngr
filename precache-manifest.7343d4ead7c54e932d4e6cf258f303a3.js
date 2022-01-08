d00164c9e517eabf481eeb4ef177c14956181409
harsh12chauhan <harsh.chauhan@ssipmt.com>
2022-01-09 00:01:54 +0530

    Updates

---
+++ precache-manifest.7343d4ead7c54e932d4e6cf258f303a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfe0ff793cdafae5ecbe640af35a5ea",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7e6a64cc238db5bd7055",
+    "url": "/my-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/my-app/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "e04ab51baa6a52428e47",
+    "url": "/my-app/static/js/3.bfe9f187.chunk.js"
+  },
+  {
+    "revision": "7e6a64cc238db5bd7055",
+    "url": "/my-app/static/js/main.9c53f426.chunk.js"
+  },
+  {
+    "revision": "629c44cf59e1568291cb",
+    "url": "/my-app/static/js/runtime~main.46dddffb.js"
+  }
+]);
